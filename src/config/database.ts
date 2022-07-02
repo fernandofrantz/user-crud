cf04bae64c909e019405910fc1c3d3b3a491693a
Fernando Frantz <fernandofrantz12@gmail.com>
2022-07-02 16:34:25 -0300

    fix: added a default user to database to fix type errors

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,1 +1,11 @@
-export const database = [{}];
+export const database = [
+  {
+    id: "0",
+    name: "admin",
+    email: "admin@admin.com",
+    password: "$admin",
+    isAdm: false,
+    createdAt: "7/2/2022, 4:04:35 PM",
+    dataField: [],
+  },
+];

6f7eae8bf23af7ce4a978f16f1895c8c4a246948
Fernando Frantz <fernandofrantz12@gmail.com>
2022-07-02 16:34:52 -0300

    feature: added new user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,17 +1,19 @@
 import express from "express";
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/userController";
+import { createUser, getUserById, getUsers, login } from "../controllers/userController";
 import { validateSchema } from "../middlewares/validateSchema";
 import { userSchema } from "../models/userSchema";
 
 const route = Router();
 
 export const userRoutes = (app: express.Application) => {
-  
   route.post("/register", createUser);
-  
-  route.get("/", getUsers);
-  
-  app.use( "/users", route );
-};
 
+  route.get("/", getUsers);
+
+  route.get("/:id", getUserById);
+
+  route.post("/login", login);
+
+  app.use("/users", route);
+};
